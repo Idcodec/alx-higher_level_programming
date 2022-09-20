abf6fe7c0854863d086d82a7e19d6646ddcb8248
Idcode <Idahosabusiness@gmail.com>
2022-09-20 07:36:52 +0000

    Copy a string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    New_word = ""
+    new_word = ""
 
     for i in range(len(str)):
         if i != n:
-            New_word += str[i]
+            new_word += str[i]
 
-	return New_word
+    return new_word
